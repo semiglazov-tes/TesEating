4381af8e308771d5524372d4cffcafd3a1626e83
semiglazov-tes <semiglazov_tes@mail.ru>
2021-06-03 22:46:31 +0300

    Добавление базового класса ViewModel

---
+++ ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TesEating.Annotations;
+
+namespace TesEating.ViewModel.Base
+{
+    internal abstract class ViewModelBase: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesEating.ViewModel
+{
+    class MainWindowViewModel
+    {
+    }
+}
